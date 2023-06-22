41520a1f40d994105fe7f905683b2147f3f9f47c
sidra3232 <125398956+sidra3232@users.noreply.github.com>
2023-06-22 18:38:57 +0100

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,16 @@
+// Create function addUser()
+function ____ {
+  // Get value of user by id player1_name_input and player2_name_input
+value="player" id="player1_name_input" id="player2_name_input"
+  // Store these values locally
+
+  //Assign "game_page.html" to window.location
+  window.location="game_page.html"
+}
+
+function send()
+{
+  number1 = document.getElementById("number1").value;
+  number2 = document.getElementById("number2").value;
+  actual_answer = parseInt(number1) * parseInt(number2);
+}
